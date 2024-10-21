18ebef0091f92bed94b31f5dc5606b158b3e0b60
bibis <167935451+xbibis@users.noreply.github.com>
2024-10-21 10:21:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática Avançada',
+    'Quanto é 2+3?',
+    '2+3 é igual a 5'
+)
+
+criaCartao(
+    'Conhecimentos da Fauna',
+    'Que animal tem 4 menbros, porém só anda com 2?',
+    'O animal é a loba Gabriel que habita no Morro doce'
+)
+
+criaCartao(
+    'Repertório sócio-cultural',
+    'Qual é o nome da namorada do Bibble em "Barbie Farytopia"?',
+    ' Seu nome é dizzle'
+)
+
+criaCartao(
+    'Eventos e Mudanças Climáticas',
+    'O que provoca a Precipitação (chuva)?',
+    'Obviamente é a fada da água que inclusive aparece no filme Tinker Bell, SilverMist'
+)
